7b085732679ad30520ae18050752e9a0c24d3ab4
Goossens Levi <Levi.Goossens@student.pxl.be>
2026-04-20 12:00:50 +0200

    add project

---
+++ TodoApp.UI.BlazorWebApp/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Application.Services;
+using TodoApp.Infrastructure.Data;
+using TodoApp.UI.BlazorWebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var connectionString = builder.Configuration.GetConnectionString("PxlConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(connectionString));
+
+// builder.Services.AddScoped<TodoAppService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
